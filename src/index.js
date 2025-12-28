25421902e9f3d2773e19cfd90616a19eb98f6f4a
Ayanami Rei <103999570+Irisbusch@users.noreply.github.com>
2025-12-28 17:25:08 +0800

    Fix import statement comment in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // 这行会寻找 App.js
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
